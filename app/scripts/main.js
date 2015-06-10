df9ea6bf9ee7d38a48a2491df77862554ab76018
Alvaro Rodriguez <alvarorodriguezmn@gmail.com>
2015-06-10 21:08:51 +0200

    repartir mano

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,31 @@
 // A $( document ).ready() block.
 $(document).ready(function() {
 
+  var $stopBtn = $('#stop-btn'),
+    $giveToP2 = $('#give-to-player-2-btn');
+
+  $stopBtn.click(function(){
+    var givenCard = giveCard(hand1);
+    console.log(givenCard);
+    $('#card-zone-casino').append(givenCard.value, givenCard.type);
+    var cardValues = $('#cards-values-casino').text();
+    cardValues = parseInt(cardValues) + givenCard.value;
+    console.log(cardValues);
+
+    $('#cards-values-casino').text(cardValues);
+  });
+
+  $giveToP2.click(function(){
+    var givenCard = giveCard(hand2);
+    console.log(givenCard);
+    $('#card-zone-player').append(givenCard.value, givenCard.type);
+    var cardValues = $('#cards-values-player').text();
+    cardValues = parseInt(cardValues) + givenCard.value;
+    console.log(cardValues);
+
+    $('#cards-values-player').text(cardValues);
+  });
+
   var casinoName = 'Croupier',
     playerName = 'Avaró';
 
@@ -28,9 +53,6 @@ $(document).ready(function() {
   createTenCards('diamonds');
   createTenCards('clubs');
 
-  console.log(cards);
-
-
   function mixCards(){
     cards.sort(function(a, b){
       return .5 - Math.random();
@@ -39,15 +61,30 @@ $(document).ready(function() {
 
   mixCards();
 
-  console.log(cards);
+  var hand1 = [],
+    hand2 = [];
 
+  function giveCard(playerHand){
 
-  /*function sayHello(name, nick){
-    nick = nick || 'yeah'
-    console.log('Hello World ' + name + nick);
+    if(cards.length > 0){
+      var card = cards.pop();
+      playerHand.push(card);
+      return card;
+    }
+    console.log(hand1, hand2, cards);
+
+    return false;
   }
 
-  sayHello(playerName);
+  //console.log(cards);
+
+
+  function sayHello(){
+
+    console.log('Hello World ');
+  }
+
+  /*sayHello(playerName);
   sayHello(casinoName);
   sayHello('rita', 'la cantaora');
   sayHello(4);*/
